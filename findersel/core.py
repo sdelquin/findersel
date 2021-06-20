061aa1c0146495819dfce75d36a3a7859fcd9402
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-06-20 12:57:53 +0100

    Fix spliting output from applescript

--- findersel/core.py
+++ findersel/core.py
@@ -6,4 +6,4 @@ from findersel import settings
 def get_selected_files():
     cmd = f'{settings.APPLESCRIPT_LAUNCHER} {settings.APPLESCRIPT_PATH}'
     output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    yield from output.decode('utf-8').strip().split('\n')
+    yield from output.decode('utf-8').splitlines()

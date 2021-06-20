6eb69fb4aa54368ac128509fd58d4b1fd324ac62
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-06-20 13:08:07 +0100

    Fix bug when no files are selected

--- findersel/core.py
+++ findersel/core.py
@@ -5,5 +5,12 @@ from findersel import settings
 
 def get_selected_files():
     cmd = f'{settings.APPLESCRIPT_LAUNCHER} {settings.APPLESCRIPT_PATH}'
-    output = subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    yield from output.decode('utf-8').splitlines()
+    try:
+        output = (
+            subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
+            .decode('utf-8')
+            .splitlines()
+        )
+    except subprocess.CalledProcessError:
+        output = []
+    yield from output

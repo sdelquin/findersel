bc622edf196c24df7d881453a947876283ee24d1
Sergio Delgado Quintero <sdelquin@gmail.com>
2021-06-20 14:08:17 +0100

    Get rid of prettyconf external dependency

--- findersel/settings.py
+++ findersel/settings.py
@@ -1,9 +1,7 @@
 from pathlib import Path
 
-from prettyconf import config
-
 PACKAGE_DIR = Path(__file__).parent
 
-APPLESCRIPT_LAUNCHER = config('APPLESCRIPT_LAUNCHER', default='osascript')
-APPLESCRIPT_NAME = config('APPLESCRIPT_NAME', default='findersel.applescript')
+APPLESCRIPT_LAUNCHER = 'osascript'
+APPLESCRIPT_NAME = 'findersel.applescript'
 APPLESCRIPT_PATH = PACKAGE_DIR / 'scripts' / APPLESCRIPT_NAME
